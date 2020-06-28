0769bdb99c4be7fc7fe6b34596a9de5fabe6a557
vovinov <v.v.ovinov@yandex.ru>
2020-06-28 22:30:29 +0300

    Updates

---
+++ precache-manifest.84654e312fa443a8e4f2656b2fb0d162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ef33170e8704d694340cbd79eda003",
+    "url": "/elfsight-test/index.html"
+  },
+  {
+    "revision": "9b15049198f39a825485",
+    "url": "/elfsight-test/static/css/main.6de40522.chunk.css"
+  },
+  {
+    "revision": "33edd41a3bd352865870",
+    "url": "/elfsight-test/static/js/2.c6f28c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elfsight-test/static/js/2.c6f28c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b15049198f39a825485",
+    "url": "/elfsight-test/static/js/main.940d9ad0.chunk.js"
+  },
+  {
+    "revision": "5f4434fd6c1ba8f90d33",
+    "url": "/elfsight-test/static/js/runtime-main.31ddeeb2.js"
+  }
+]);
