81cf33e22b8688e3c2778fc2df815625e2191ced
vovinov <v.v.ovinov@yandex.ru>
2020-06-28 22:35:39 +0300

    fix no-cover picture path

--- src/components/albumItem/AlbumItem.js
+++ src/components/albumItem/AlbumItem.js
@@ -7,7 +7,7 @@ export default function AlbumItem({album, onAlbumClick, count}) {
     return (
         <li className="albums__item card" onClick={onAlbumClick}>
             <div className="card__cover">
-                <img className="card__img" src="../../../images/no-cover.png" alt="Cover" />
+                <img className="card__img" src="./images/no-cover.png" alt="Cover" />
                 <div className="card__count">{count}</div> 
             </div>
             <h2 className="card__title">{album.title}</h2>                
